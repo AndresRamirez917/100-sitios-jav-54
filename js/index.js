640eb507feb17b02badd83a7e53aee9ad38a093e
AndresRamirez917 <andresmintic2021@gmail.com>
2024-10-26 19:42:58 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 export async function getData() {
     const result = await fetch('https://fakestoreapi.com/products/');
     const products = await result.json();
+    const arr = ["project-1.html", "project-2.html", "project-3.html", "project-4.html"]
     console.log(products);
     const productsArr = products.map(elemento => Object.entries(elemento));
     console.log(productsArr)
@@ -10,33 +11,35 @@ export async function getData() {
         const randInt = randomData(1, productsArr.length);
         const randIndex = randInt;
         for(let i = 0; i < productsArrSliced.length; i++){
-            if(element.id == i){
-                const grid_box = document.createRange().createContextualFragment(`
-                    
-            <div class="grid-box grid-box-${i}">
-                <div class="overlay overlay-${i}">
-                    <div class="image"> 
-                        <img src="${productsArr[randIndex][5][1]}">
+            for(let j = 0; j < i; j++){
+                if(element.id == i){
+                    const grid_box = document.createRange().createContextualFragment(`
+                        
+                <div class="grid-box grid-box-${i}">
+                    <div class="overlay overlay-${i}">
+                        <div class="image"> 
+                            <img src="${productsArr[randIndex][5][1]}">
+                        </div>
+                        <h4>Magna</h4>
+                        <p >
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, perspiciatis? Sed maxime fugiat ea itaque
+                            aut aliquid natus sapiente numquam, totam recusandae consectetur placeat quibusdam?
+                        </p>
+                        <a href="${arr[j]}" class="btn button-1">Learn More</a>
                     </div>
-                    <h4>Magna</h4>
-                    <p >
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, perspiciatis? Sed maxime fugiat ea itaque
-                        aut aliquid natus sapiente numquam, totam recusandae consectetur placeat quibusdam?
-                    </p>
-                    <a href="project-1.html" class="btn button-1">Learn More</a>
                 </div>
-            </div>
-                    
-                    
-                    `)
-                    const camaras = document.getElementById('cameras');
-                    camaras.append(grid_box)
-                    console.log(productsArr[randIndex][5][1])
-                }
-                //return console.log("madresita")
+                        
+                        
+                        `)
+                        const camaras = document.getElementById('cameras');
+                        camaras.append(grid_box)
+                    }
             }
-            return productsArr[randIndex][5][1]
+            }
+            
+            
+            return `<img src="/images/image-1.jpg"`
         });
-}
+    }
 
 getData()
